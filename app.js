1c88b97b9f08427234128ca963d89281f482b82b
Anjani <anjanivaranasi123@gmail.com>
2021-07-23 01:47:00 +0530

    Added Compose

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const lodash = require('lodash');
+const _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -54,18 +54,22 @@ app.post("/compose",function(req,res){
 });
 
 app.get("/posts/:postName",function(req,res){
-  var requestedCon = req.params.postName;
+  var requestedCon = _.lowerCase(req.params.postName);
 //foreach can also be used instead of for loop !
-  for(var i =0;i<posts.length;i++)
+
+
+  for(var i = 0;i<posts.length; i++)
   {
-    if(posts[i].title === requestedCon) { 
-      console.log("Match Found !") 
+    if( _.lowerCase(posts[i].title) === requestedCon) { 
+      res.render("posts",{
+        titlep : posts[i].title,
+        bodyp : posts[i].body
+      });
+       
     }
-     else{ 
-      console.log("Not Found !")
-     }
   }
 });
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
